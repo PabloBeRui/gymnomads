4425fdf2cec076b05d4bb04058b7bc26599a63f6
PabloBeRui <121795105+PabloBeRui@users.noreply.github.com>
2025-10-17 10:09:56 +0200

    feat(gyms): add city filtering to getAllGyms endpoint
    - Modified the 'getAllGyms' controller to accept an optional 'city' query parameter for filtering results.
    - Fixed a missing 'mysql2' dependency in the backend's package.json.

--- backend/src/controllers/gym-controller.js
+++ backend/src/controllers/gym-controller.js
@@ -1,28 +1,26 @@
 const db = require("../../config/db");
 
-// Función para obtener todos los gimnasios
-// Function to get all gyms
+// Función para obtener todos los gimnasios o filtrar gimansio por ciudad
+// Function to get all gyms or filter by city
 
 const getAllGyms = async (req, res) => {
   try {
-    // Ejecutar la consulta SQL usando el pool de conexiones
-    // Execute the SQL query using the connection pool
-    // const rows = queryResults[0]
+    const { city } = req.query;
 
-    const [rows] = await db.query("SELECT * FROM gyms");
+    let query = "SELECT * FROM gyms";
+    const params = [];
 
-    // Responder con un estado 200 (OK) y los datos en formato JSON
-    // Respond with a 200 status (OK) and the data in JSON format
+    if (city) {
+      // Si hay filtro, añade WHERE // If there is a filter, add WHERE
+      query += " WHERE city = ?";
+      params.push(city);
+    }
 
+    const [rows] = await db.query(query, params);
     res.status(200).json(rows);
   } catch (error) {
-    // en caso de error mostrar en consola y enviar un error 500
-    // In case of error, display in console and send a 500 error
-
-    console.error(error);
-    res.status(500).json({
-      message: `Error en el servidor`,
-    });
+    console.error(`Error: ${error}`);
+    res.status(500).json({ message: "error interno del servidor" });
   }
 };
 
